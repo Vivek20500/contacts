1259d118fdd8fb19d8c433fa0b17f42fe8ef7717
Vivek20500 <vivekverma20500@gmail.com>
2024-05-04 17:20:18 +0530

    first change

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "./card";
+import Header from "./header";
+import "./App.css"
+
+function App(){
+    return (
+        <div className="next">
+            <Header />
+            <div className="card-cont">
+                <Card name="Deadpool" phone="1234 2344 23" img="https://source.unsplash.com/random/?anime"/>
+                <Card name="Spiderman" phone="1234 7654 23" img="https://source.unsplash.com/random/?india"/>
+                <Card name="Black widow" phone="1234 2884 23" img="https://source.unsplash.com/random/?colour"/>
+            </div>
+        </div>
+    )
+}
+export default App;
---
+++ src/card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const h=200+"px";
+function Card(props){
+    return(
+        <div className="card">
+            <img height={h} width={h} src={props.img} alt={"hello"} ></img>
+            <p className="name">{props.name}</p>
+            <p>{props.phone}</p>
+        </div>);
+}
+export default Card;
